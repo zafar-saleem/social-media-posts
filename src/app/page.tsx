9fe1fb5edadd66a25ed91798407c40a38bbc8a85
Zafar Saleem <zafarsaleem3@gmail.com>
2025-05-09 12:44:23 +0100

    feat: notification box case study

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,14 @@
 import styles from "./page.module.css";
 import React from "react";
 import { ButtonsWithState } from "@/components/buttons/state-buttons";
+import { Outdoor } from "@/components/cards/Outdoor";
+import { Notifications } from "@/components/notifications";
 
 export default function Home() {
   return (
     <main className={styles.center}>
       {/* <MegaMenu /> */}
-      <ButtonsWithState />
+      <Notifications />
     </main>
   );
 }
