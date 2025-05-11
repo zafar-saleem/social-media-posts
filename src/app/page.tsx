1c0dcbe42060b2103b0e2fed5ea4bac41daead15
Zafar Saleem <zafarsaleem3@gmail.com>
2025-05-11 12:35:46 +0100

    feat: reaction icon buttons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,14 @@
 "use client"
 
+import { Reaction } from "@/components/buttons/reaction";
 import styles from "./page.module.css";
 import React from "react";
-import { ButtonsWithState } from "@/components/buttons/state-buttons";
-import { Outdoor } from "@/components/cards/Outdoor";
-import { Notifications } from "@/components/notifications";
 
 export default function Home() {
   return (
     <main className={styles.center}>
       {/* <MegaMenu /> */}
-      <Notifications />
+      <Reaction />
     </main>
   );
 }
