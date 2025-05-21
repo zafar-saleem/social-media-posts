45746f94289659c736184a145e6d8386d106171c
Zafar Saleem <zafarsaleem3@gmail.com>
2025-05-21 16:11:09 +0100

    feat: membership selection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
 "use client"
 
-import { Ticket } from "@/components/ticket";
+import { Membership } from "@/components/membership";
 import styles from "./page.module.css";
 import React from "react";
 
 export default function Home() {
   return (
     <main className={styles.center}>
-      <Ticket />
+      <Membership />
     </main>
   );
 }
