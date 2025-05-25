6830ca31dfd9821dd18cc1f393efa9487b41e72c
Zafar Saleem <zafarsaleem3@gmail.com>
2025-05-25 14:20:00 +0100

    feat: faq

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
 "use client"
 
-import { SwitchAccount } from "@/components/switch-account";
+import { FAQ } from "@/components/faq";
 import styles from "./page.module.css";
 import React from "react";
 
 export default function Home() {
   return (
     <main className={styles.center}>
-      <SwitchAccount />
+      <FAQ />
     </main>
   );
 }
