e0e4f50a61b7de7a99490cd8ef43f0ebbb851a68
Zafar Saleem <zafarsaleem3@gmail.com>
2025-05-25 07:32:16 +0100

    feat: switch account menu

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
 "use client"
 
-import { Membership } from "@/components/membership";
+import { SwitchAccount } from "@/components/switch-account";
 import styles from "./page.module.css";
 import React from "react";
 
 export default function Home() {
   return (
     <main className={styles.center}>
-      <Membership />
+      <SwitchAccount />
     </main>
   );
 }
