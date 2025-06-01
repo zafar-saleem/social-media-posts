f5f7b4ab0476136a8dd28b4bd1f7a1f78dc61c6b
Zafar Saleem <zafarsaleem3@gmail.com>
2025-06-01 10:56:06 +0100

    feat: destination component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,12 @@
-"use client"
-
-import { FAQ } from "@/components/faq";
 import styles from "./page.module.css";
 import React from "react";
+import { Navbar } from "@/components/nav/nav-bar";
+import { TodoDoneEffect } from "@/components/todo-done";
 
 export default function Home() {
   return (
     <main className={styles.center}>
-      <FAQ />
+      <TodoDoneEffect />
     </main>
   );
 }
