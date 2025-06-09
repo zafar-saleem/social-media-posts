bfaf8a1684e94ed70f9a594700ae8709cde9728a
Zafar Saleem <zafarsaleem3@gmail.com>
2025-06-09 15:05:02 +0100

    fix: fixed responsiveness in storage plan component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,12 @@ import styles from "./page.module.css";
 import React from "react";
 import { Navbar } from "@/components/nav/nav-bar";
 import { Fitness } from "@/components/cards/fitness";
+import { StoragePlan } from "@/components/storage-plan";
 
 export default function Home() {
   return (
     <main className={styles.center}>
-      <Fitness />
+      <StoragePlan />
     </main>
   );
 }
