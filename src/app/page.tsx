0acfa531127af981affcffde44ae02a6a98c502e
Zafar Saleem <zafarsaleem3@gmail.com>
2025-06-12 11:47:51 +0100

    feat: account setup component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
+import { AccountSetup } from "@/components/account-setup";
 import styles from "./page.module.css";
 import React from "react";
-import { ShareLink } from "@/components/share-link";
 
 export default function Home() {
   return (
     <main className={styles.center}>
-      <ShareLink />
+      <AccountSetup />
     </main>
   );
 }
