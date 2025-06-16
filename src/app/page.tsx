22174a8815a7b6071d153caa5571ef8278c3daec
Zafar Saleem <zafarsaleem3@gmail.com>
2025-06-16 15:17:40 +0100

    feat: split button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
+import { SplitButton } from "@/components/buttons/split-button";
 import styles from "./page.module.css";
 import React from "react";
-import { Meeting } from "@/components/meeting";
 
 export default function Home() {
   return (
     <main className={styles.center}>
-      <Meeting />
+      <SplitButton />
     </main>
   );
 }
