4b735a85530966eb37113bc8bca1bed36d2cf9c0
Zafar Saleem <zafarsaleem3@gmail.com>
2025-06-27 13:22:20 +0100

    feat: cta button added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
-import { CardAnimation } from "@/components/cards/card-animation";
+import { PerspectiveEffect } from "@/components/buttons/perspective-effect";
 import styles from "./page.module.css";
 import React from "react";
+import { CTAButton } from "@/components/buttons/cta-2";
 
 export default function Home() {
   return (
     <main className={styles.center}>
-      <CardAnimation />
+      <CTAButton />
     </main>
   );
 }
