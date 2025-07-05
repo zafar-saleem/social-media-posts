0fa761f42de5c43cc22fce9b30e8205b4d2d4973
Zafar Saleem <zafarsaleem3@gmail.com>
2025-07-05 11:54:53 +0100

    feat: login component added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
+import { Login } from "@/components/login";
 import styles from "./page.module.css";
 import React from "react";
-import { RecentWorkCard } from "@/components/cards/work";
 
 export default function Home() {
   return (
     <main className={styles.center}>
-      <RecentWorkCard />
+      <Login />
     </main>
   );
 }
