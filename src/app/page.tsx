4e8b7e0df2a89ff9eed55bd9f9ca4f8f95ac738f
Zafar Saleem <zafarsaleem3@gmail.com>
2025-07-06 12:04:27 +0100

    feat: pricing plan component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-import { Login } from "@/components/login";
 import styles from "./page.module.css";
 import React from "react";
+import { PricingPlan } from "@/components/pricing";
 
 export default function Home() {
   return (
     <main className={styles.center}>
-      <Login />
+      <PricingPlan />
     </main>
   );
 }
