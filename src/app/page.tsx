71810bc4c85e44edf45489bc2184967a6143af69
Zafar Saleem <zafarsaleem3@gmail.com>
2025-07-06 13:05:18 +0100

    feat: mention card component added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 import styles from "./page.module.css";
 import React from "react";
-import { PricingPlan } from "@/components/pricing";
+import { MentionCard } from "@/components/cards/card-mention";
 
 export default function Home() {
   return (
     <main className={styles.center}>
-      <PricingPlan />
+      <MentionCard />
     </main>
   );
 }
