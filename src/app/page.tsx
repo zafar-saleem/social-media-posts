ab10192de92e0dc386ea59f4517dfb3074fd64b0
Zafar Saleem <zafarsaleem3@gmail.com>
2025-08-03 12:33:04 +0100

    feat: todo list added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 import styles from "./page.module.css";
 import React from "react";
-import { Billing } from "@/components/2025/week-32/billing";
+import { TodoList } from "@/components/2025/week-32/todo-list";
 
 export default function Home() {
   return (
     <main className={styles.center}>
-      <Billing />
+      <TodoList />
     </main>
   );
 }
