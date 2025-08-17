ea4cfa5069aae19fa250d1275c69aed30f4ce4f6
Zafar Saleem <zafarsaleem3@gmail.com>
2025-08-17 13:10:00 +0100

    feat: integration component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 import styles from "./page.module.css";
 import React from "react";
-import { AddPaymentMethod } from "@/components/2025/week-34/payment";
+import { IntegratePlatforms } from "@/components/2025/week-34/integrate-platforms";
 
 export default function Home() {
   return (
     <main className={styles.center}>
-      <AddPaymentMethod />
+      <IntegratePlatforms />
     </main>
   );
 }
