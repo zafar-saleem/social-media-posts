4565e6bf9a6ea23ee33761a6a269eb0a0513e36a
Zafar Saleem <zafarsaleem3@gmail.com>
2025-08-24 08:50:08 +0100

    feat: moved form component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import styles from "./page.module.css";
 import React from "react";
-import { SummaryTask } from "@/components/2025/week-34/summery-task";
 import { CTA } from "@/components/2025/week-35/cta";
 
 export default function Home() {
