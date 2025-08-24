c1ed29f8587b0c8c805a913c25db2d713d14f857
Zafar Saleem <zafarsaleem3@gmail.com>
2025-08-24 18:51:30 +0100

    refactor: replace bio photo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import styles from "./page.module.css";
 import React from "react";
 import { Pricing } from "@/components/2025/week-35/pricing";
+import { Bio } from "@/components/2025/week-35/bio-card";
 
 export default function Home() {
   return (
     <main className={styles.center}>
-      <Pricing />
+      <Bio />
     </main>
   );
 }
--- src/components/2025/week-35/bio-card/index.tsx
+++ src/components/2025/week-35/bio-card/index.tsx
@@ -8,7 +8,7 @@ export const Bio = () => {
         <div className={styles.metadata}>
           <div>
             <span className={styles.name}>Zafar Saleem</span>
-            <span className={styles.role}>CEO</span>
+            <span className={styles.role}>Engineer</span>
           </div>
           <div className={styles.controls}>
             <label className={styles.btn_toggle}>
@@ -18,7 +18,7 @@ export const Bio = () => {
               </span>
               <input type="checkbox" className={styles.button} />
               <span className={styles.icon}>
-                <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="rgb(20 22 31)" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className={`lucide lucide-chevron-down-icon lucide-chevron-down ${styles.arrow}`}>
+                <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="rgb(20 22 31)" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className={`lucide lucide-chevron-down-icon lucide-chevron-down ${styles.arrow}`}>
                   <path d="m6 9 6 6 6-6"/>
                 </svg>
               </span>
@@ -32,19 +32,19 @@ export const Bio = () => {
         </p>
         <div className={styles.socials}>
           <Link href="#" className={styles.socials_linkedin}>
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="rgb(159 255 0)" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-linkedin-icon lucide-linkedin">
+            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="rgb(159 255 0)" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-linkedin-icon lucide-linkedin">
               <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"/>
               <rect width="4" height="12" x="2" y="9"/>
               <circle cx="4" cy="4" r="2"/>
             </svg>
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="rgb(159 255 0)" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-linkedin-icon lucide-linkedin">
+            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="rgb(159 255 0)" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-linkedin-icon lucide-linkedin">
               <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"/>
               <rect width="4" height="12" x="2" y="9"/>
               <circle cx="4" cy="4" r="2"/>
             </svg>
           </Link>
           <Link href="#" className={styles.socials_insta}>
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="rgb(159 255 0)" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-instagram-icon lucide-instagram">
+            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="rgb(159 255 0)" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-instagram-icon lucide-instagram">
               <rect width="20" height="20" x="2" y="2" rx="5" ry="5"/>
               <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"/>
               <line x1="17.5" x2="17.51" y1="6.5" y2="6.5"/>
