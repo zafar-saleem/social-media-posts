bfce1ec73eb45c1c119270a1ee19a414066740b7
Zafar Saleem <zafarsaleem3@gmail.com>
2025-08-24 18:55:30 +0100

    feat: glitch fix

--- src/components/2025/week-35/bio-card/index.tsx
+++ src/components/2025/week-35/bio-card/index.tsx
@@ -49,6 +49,11 @@ export const Bio = () => {
               <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"/>
               <line x1="17.5" x2="17.51" y1="6.5" y2="6.5"/>
             </svg>
+            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="rgb(159 255 0)" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-instagram-icon lucide-instagram">
+              <rect width="20" height="20" x="2" y="2" rx="5" ry="5"/>
+              <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"/>
+              <line x1="17.5" x2="17.51" y1="6.5" y2="6.5"/>
+            </svg>
           </Link>
         </div>
       </div>
