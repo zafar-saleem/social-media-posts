93779ef297122296afff36f65cc6676e94595131
Zafar Saleem <zafarsaleem3@gmail.com>
2025-08-31 13:00:27 +0100

    feat: coffee picker component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 import styles from "./page.module.css";
 import React from "react";
-import { TimePicker } from "@/components/2025/week-36/time-picker";
+import { CoffeePicker } from "@/components/2025/week-36/coffee-picker";
 
 export default function Home() {
   return (
     <main className={styles.center}>
-      <TimePicker />
+      <CoffeePicker />
     </main>
   );
 }
