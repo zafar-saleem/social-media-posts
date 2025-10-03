01a103463c0d7b414efa022f72d6269124a994a3
Zafar Saleem <zafarsaleem3@gmail.com>
2025-10-03 10:00:37 +0100

    feat: docs and hover effect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import styles from "./page.module.css";
 import React from "react";
 import { WeeklyFitnessTracker } from "@/components/2025/week-41/FitnessTracker";
+import { Assigned } from "@/components/2025/week-41/assignment";
 
 export default function Home() {
   return (
     <main className={styles.center}>
-      <WeeklyFitnessTracker />
+      <Assigned />
     </main>
   );
 }
