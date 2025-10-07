fa3fa876c3843f536b1c889513ea7781c34bc411
Zafar Saleem <zafarsaleem3@gmail.com>
2025-10-07 11:32:08 +0100

    feat: product card tooltip improvements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 import styles from "./page.module.css";
 import React from "react";
-import { Input } from "@/components/2025/week-41/input";
+import { ProductCard } from "@/components/2025/week-41/product-card";
 
 export default function Home() {
   return (
     <main className={styles.center}>
-      <Input />
+      <ProductCard />
     </main>
   );
 }
