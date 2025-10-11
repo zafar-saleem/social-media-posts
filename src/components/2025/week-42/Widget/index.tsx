495824eda32d173251743345c722cbfad7abf110
Zafar Saleem <zafarsaleem3@gmail.com>
2025-10-11 13:58:17 +0100

    feat: tooltip added

--- src/components/2025/week-42/Widget/index.tsx
+++ src/components/2025/week-42/Widget/index.tsx
@@ -2,30 +2,35 @@
 
 import React from "react";
 import styles from "./Widget.module.css";
+import Link from "next/link";
 
 const People = [
   {
     avatar: <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="rgb(96 103 102)" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-shield-user-icon lucide-shield-user"><path d="M20 13c0 5-3.5 7.5-7.66 8.95a1 1 0 0 1-.67-.01C7.5 20.5 4 18 4 13V6a1 1 0 0 1 1-1c2 0 4.5-1.2 6.24-2.72a1.17 1.17 0 0 1 1.52 0C14.51 3.81 17 5 19 5a1 1 0 0 1 1 1z"/><path d="M6.376 18.91a6 6 0 0 1 11.249.003"/><circle cx="12" cy="11" r="4"/></svg>,
     role: "CEO",
     name: "Jone Krookes",
+    email: "jone@email.com",
     icon: <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="rgb(96 103 102)" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-mail-icon lucide-mail"><path d="m22 7-8.991 5.727a2 2 0 0 1-2.009 0L2 7"/><rect x="2" y="4" width="20" height="16" rx="2"/></svg>
   },
   {
     avatar: <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="rgb(96 103 102)" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-user-cog-icon lucide-user-cog"><path d="M10 15H6a4 4 0 0 0-4 4v2"/><path d="m14.305 16.53.923-.382"/><path d="m15.228 13.852-.923-.383"/><path d="m16.852 12.228-.383-.923"/><path d="m16.852 17.772-.383.924"/><path d="m19.148 12.228.383-.923"/><path d="m19.53 18.696-.382-.924"/><path d="m20.772 13.852.924-.383"/><path d="m20.772 16.148.924.383"/><circle cx="18" cy="15" r="3"/><circle cx="9" cy="7" r="4"/></svg>,
     role: "PM",
     name: "Pherguson Lests",
+    email: "pherguson@email.com",
     icon: <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="rgb(96 103 102)" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-mail-icon lucide-mail"><path d="m22 7-8.991 5.727a2 2 0 0 1-2.009 0L2 7"/><rect x="2" y="4" width="20" height="16" rx="2"/></svg>
   },
   {
     avatar: <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="rgb(96 103 102)" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-user-cog-icon lucide-user-cog"><path d="M10 15H6a4 4 0 0 0-4 4v2"/><path d="m14.305 16.53.923-.382"/><path d="m15.228 13.852-.923-.383"/><path d="m16.852 12.228-.383-.923"/><path d="m16.852 17.772-.383.924"/><path d="m19.148 12.228.383-.923"/><path d="m19.53 18.696-.382-.924"/><path d="m20.772 13.852.924-.383"/><path d="m20.772 16.148.924.383"/><circle cx="18" cy="15" r="3"/><circle cx="9" cy="7" r="4"/></svg>,
     role: "XYZ",
     name: "Zafar Saleem",
+    email: "zafar@email.com",
     icon: <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="rgb(96 103 102)" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-mail-icon lucide-mail"><path d="m22 7-8.991 5.727a2 2 0 0 1-2.009 0L2 7"/><rect x="2" y="4" width="20" height="16" rx="2"/></svg>
   },
   {
     avatar: <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="rgb(96 103 102)" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-user-cog-icon lucide-user-cog"><path d="M10 15H6a4 4 0 0 0-4 4v2"/><path d="m14.305 16.53.923-.382"/><path d="m15.228 13.852-.923-.383"/><path d="m16.852 12.228-.383-.923"/><path d="m16.852 17.772-.383.924"/><path d="m19.148 12.228.383-.923"/><path d="m19.53 18.696-.382-.924"/><path d="m20.772 13.852.924-.383"/><path d="m20.772 16.148.924.383"/><circle cx="18" cy="15" r="3"/><circle cx="9" cy="7" r="4"/></svg>,
     role: "ABC",
-    name: "M Saleem Khan",
+    name: "Saleem Khan",
+    email: "saleem@email.com",
     icon: <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="rgb(96 103 102)" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-mail-icon lucide-mail"><path d="m22 7-8.991 5.727a2 2 0 0 1-2.009 0L2 7"/><rect x="2" y="4" width="20" height="16" rx="2"/></svg>
   },
 ]
@@ -127,6 +132,7 @@ type CardProps = ButtonProps & {
   role: string;
   icon: any;
   avatar: any;
+  email: string;
 }
 
 const Card = ({ index, ...rest }: CardProps) => {
@@ -142,9 +148,10 @@ const Card = ({ index, ...rest }: CardProps) => {
       {rest.avatar}
       <span className={styles.card_name}>{rest.name}</span>
       <span className={styles.card_role}>{rest.role}</span>
-      <button className={styles.button_email}>
+      <Link href={`mailto:${rest.email}`} className={styles.button_email}>
         {rest.icon}
-      </button>
+      </Link>
+      <span className={styles.tooltip}>Email {rest.name.split(" ")[0]}</span>
     </div>
   )
 }
