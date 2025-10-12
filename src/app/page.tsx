9332130a58ab1ccd11aef9b24b9426d7f0734b25
Zafar Saleem <zafarsaleem3@gmail.com>
2025-10-12 14:18:46 +0100

    feat: row colors updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 import styles from "./page.module.css";
 import React from "react";
-import { CaseStudies } from "@/components/2025/week-42/CaseStudies";
+import { ActionBar } from "@/components/2025/week-42/ActionBar";
 
 export default function Home() {
   return (
     <main className={styles.center}>
-      <CaseStudies />
+      <ActionBar />
     </main>
   );
 }
