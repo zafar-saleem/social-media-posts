b9b671050c11b6188a6911f027900aa9d940bf5b
Zafar Saleem <zafarsaleem3@gmail.com>
2025-10-16 16:37:08 +0100

    feat: cta button added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 import styles from "./page.module.css";
 import React from "react";
-import { Preloader } from "@/components/2025/week-43/Preloader";
+import { CTAButton } from "@/components/2025/week-43/cta";
 
 export default function Home() {
   return (
     <main className={styles.center}>
-      <Preloader />
+      <CTAButton />
     </main>
   );
 }
