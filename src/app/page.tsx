b5a88e8c236c600779a314b2e96b913a7fdd312d
Zafar Saleem <zafarsaleem3@gmail.com>
2025-10-17 10:31:08 +0100

    feat: expandable button added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 import styles from "./page.module.css";
 import React from "react";
-import { CTAButton } from "@/components/2025/week-43/cta";
+import { ExpandableMore } from "@/components/2025/week-44/ExpandMore";
 
 export default function Home() {
   return (
     <main className={styles.center}>
-      <CTAButton />
+      <ExpandableMore />
     </main>
   );
 }
