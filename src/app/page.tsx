2741ac78a2d3c3fba56585eb4edef7db24cd2615
Zafar Saleem <zafarsaleem3@gmail.com>
2025-10-18 16:09:27 +0100

    feat: step indicator added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-import { Notifications } from "@/components/2025/week-43/Notifications";
 import styles from "./page.module.css";
 import React from "react";
+import { StepIndicators } from "@/components/2025/week-44/StepIndicators";
 
 export default function Home() {
   return (
     <main className={styles.center}>
-      <Notifications />
+      <StepIndicators />
     </main>
   );
 }
