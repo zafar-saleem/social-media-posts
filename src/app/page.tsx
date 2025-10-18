b063bdafacd6b2f65970accc17090c5840fbb39f
Zafar Saleem <zafarsaleem3@gmail.com>
2025-10-18 09:19:46 +0100

    fix: fixed active state

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 import styles from "./page.module.css";
 import React from "react";
-import { BookingCard } from "@/components/2025/week-43/booking-card";
+import { NewChannel } from "@/components/2025/week-43/NewChannel";
 
 export default function Home() {
   return (
     <main className={styles.center}>
-      <BookingCard />
+      <NewChannel />
     </main>
   );
 }
