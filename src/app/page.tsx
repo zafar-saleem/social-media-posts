f5feb760300252556c800d694e777b39e74821a1
Zafar Saleem <zafarsaleem3@gmail.com>
2025-10-18 12:54:09 +0100

    feat: moved notifications to 43

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
+import { Notifications } from "@/components/2025/week-43/Notifications";
 import styles from "./page.module.css";
 import React from "react";
-import { NewChannel } from "@/components/2025/week-43/NewChannel";
 
 export default function Home() {
   return (
     <main className={styles.center}>
-      <NewChannel />
+      <Notifications />
     </main>
   );
 }
