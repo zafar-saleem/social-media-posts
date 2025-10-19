6a52fd8c3ca73f5bb77b7a8343548e05ce68788a
Zafar Saleem <zafarsaleem3@gmail.com>
2025-10-19 11:24:57 +0100

    fix: added carousel and fixed button paddings

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 import styles from "./page.module.css";
 import React from "react";
-import { ToggleSidebar } from "@/components/2025/week-43/toggle-sidebar";
+import { BookingCard } from "@/components/2025/week-43/booking-card";
 
 export default function Home() {
   return (
     <main className={styles.center}>
-      <ToggleSidebar />
+      <BookingCard />
     </main>
   );
 }
