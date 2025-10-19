60a28aa151f49ba08b45062941219e23a188d013
Zafar Saleem <zafarsaleem3@gmail.com>
2025-10-19 13:18:51 +0100

    fix: fixed blur and refactor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 import styles from "./page.module.css";
 import React from "react";
-import { BookingCard } from "@/components/2025/week-43/booking-card";
+import { Preloader } from "@/components/2025/week-43/Preloader";
 
 export default function Home() {
   return (
     <main className={styles.center}>
-      <BookingCard />
+      <Preloader />
     </main>
   );
 }
