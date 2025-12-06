c427c4d772ff17c2ebdd3736c521c4f7b4fc91e3
Zafar Saleem <zafarsaleem3@gmail.com>
2025-12-06 12:27:21 +0000

    feat: refactor

--- src/components/2025/week-50/Button/index.tsx
+++ src/components/2025/week-50/Button/index.tsx
@@ -3,12 +3,8 @@
 import React from "react";
 import styles from "./styles.module.css";
 
-// <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="rgb(218 245 255)" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-arrow-down-to-line-icon lucide-arrow-down-to-line"><path d="M12 17V3"/><path d="m6 11 6 6 6-6"/><path d="M19 21H5"/></svg>
-
-export const DownloadButton = ({ label }) => {
+export const DownloadButton = ({ label }: any) => {
   const [click, setClick] = React.useState(false);
-
-  console.log(click)
 
   return (
     <button
