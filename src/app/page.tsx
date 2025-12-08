27eb5ecb9a069825e377902cd939068660c28ca0
Zafar Saleem <zafarsaleem3@gmail.com>
2025-12-08 08:24:09 +0000

    feat: enhancements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
+import { ProductListing } from "@/components/2025/week-50/ProductListing";
 import styles from "./page.module.css";
-import { Tabs } from "@/components/2025/week-50/Tabs";
 
 export default function Home() {
   return (
     <main className={styles.center}>
-      <Tabs />
+      <ProductListing />
     </main>
   );
 }
