2858eedd62f2ce7c7c610c693b94f79d0882312a
Zafar Saleem <zafarsaleem3@gmail.com>
2025-12-12 11:44:02 +0000

    feat: enhancements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import { ActivityTracker } from "@/components/2025/week-51/ActivityTracker";
 import styles from "./page.module.css";
+import { MirrorButton } from "@/components/2025/week-51/Button";
 
 export default function Home() {
   return (
     <main className={styles.center}>
-      <ActivityTracker />
+      <MirrorButton />
     </main>
   );
 }
