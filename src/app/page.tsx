a80e5bc9887d536e13616a92f5e3731fac429a23
Zafar Saleem <zafarsaleem3@gmail.com>
2025-12-16 07:06:33 +0000

    feat: product categories

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
+import { ProductCategory } from "@/components/2025/week-52/ProductCategory";
 import styles from "./page.module.css";
-import { ShippingFeaturesSection } from "@/components/2025/week-52/ShippingFeatures";
 
 export default function Home() {
   return (
     <main className={styles.center}>
-      <ShippingFeaturesSection />
+      <ProductCategory />
     </main>
   );
 }
