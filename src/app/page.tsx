43ca422c9404b3e3dd854b9f3f07f9560032be60
Zafar Saleem <zafarsaleem3@gmail.com>
2026-01-01 19:54:29 +0000

    feat: hover animation to travel card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import { ButtonSave } from "@/components/2026/1.jan/5-9/ButtonSave";
 import styles from "./page.module.css";
+import { TravelCard } from "@/components/2026/1.jan/5-9/TravelCard";
 
 export default function Home() {
   return (
     <main className={styles.center}>
-      <ButtonSave />
+      <TravelCard />
     </main>
   );
 }
