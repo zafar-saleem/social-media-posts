4788d86dc5e808cc27f4459171a16892eb75d4fd
Zafar Saleem <zafarsaleem3@gmail.com>
2026-02-08 21:55:07 +0000

    feat: box shadow removed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
+import { ProductShare } from "@/components/2026/2. Feb/09-13/ProductShare";
 import styles from "./page.module.css";
-import { StatusBoard } from "@/components/2026/2. Feb/09-13/StatusBoard";
 
 export default function Home() {
   return (
     <main className={styles.center}>
-      <StatusBoard />
+      <ProductShare />
     </main>
   );
 }
