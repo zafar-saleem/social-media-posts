08e9f45261e199405abe4af625250bd035508256
Zafar Saleem <zafarsaleem3@gmail.com>
2026-02-09 22:19:23 +0000

    feat: improved tasks list

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import { DisabledButton } from "@/components/2026/2. Feb/09-13/DisabledButton";
 import styles from "./page.module.css";
+import { TasksList } from "@/components/2026/2. Feb/09-13/TasksList";
 
 export default function Home() {
   return (
     <main className={styles.center}>
-      <DisabledButton />
+      <TasksList />
     </main>
   );
 }
