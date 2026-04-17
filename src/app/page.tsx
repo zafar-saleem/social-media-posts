a9578c935122ff363466b17b41bda3dfeccf871c
Zafar Saleem <zafarsaleem3@gmail.com>
2026-04-17 17:17:08 +0100

    feat: hint tooltip button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import { MeetingsCard } from "@/components/2026/4. Apr/13-17/MeetingsCard";
 import styles from "./page.module.css";
+import { HintTooltip } from "@/components/2026/4. Apr/20-24/HintTooltip";
 
 export default function Home() {
   return (
     <main className={styles.center}>
-      <MeetingsCard />
+      <HintTooltip />
     </main>
   );
 }
