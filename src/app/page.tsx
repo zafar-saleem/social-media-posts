b472ce322a7f41ebb22eeedbf5dc687d01207181
Zafar Saleem <zafarsaleem3@gmail.com>
2026-05-05 12:37:29 +0100

    feat: minor fixes toggle button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import { CreateNewButton } from "@/components/2026/5. May/11-15/CreateNewButton";
 import styles from "./page.module.css";
+import { ToggleButton } from "@/components/2026/5. May/03-08/ToggleButton";
 
 export default function Home() {
   return (
     <main className={styles.center}>
-      <CreateNewButton />
+      <ToggleButton />
     </main>
   );
 }
