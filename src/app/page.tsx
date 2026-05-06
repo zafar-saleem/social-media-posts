7ab42e076da30075aa35b61cf3ab39de5963f14f
Zafar Saleem <zafarsaleem3@gmail.com>
2026-05-06 13:33:35 +0100

    feat: minor adjustments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
+import { ExpandableMenu } from "@/components/2026/5. May/03-08/ExpandableMenu";
 import styles from "./page.module.css";
-import { ToggleButton } from "@/components/2026/5. May/03-08/ToggleButton";
 
 export default function Home() {
   return (
     <main className={styles.center}>
-      <ToggleButton />
+      <ExpandableMenu />
     </main>
   );
 }
