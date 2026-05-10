2aa0d704c77423eb3024ba1a384c3e880c0d60bf
Zafar Saleem <zafarsaleem3@gmail.com>
2026-05-10 13:16:29 +0100

    feat: improved interaction social buttons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import { BudgetCard } from "@/components/2026/5. May/11-15/BudgetCard";
 import styles from "./page.module.css";
+import { SocialButtons } from "@/components/2026/5. May/11-15/SocialButton";
 
 export default function Home() {
   return (
     <main className={styles.center}>
-      <BudgetCard />
+      <SocialButtons />
     </main>
   );
 }
