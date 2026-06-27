a27dbd638148250845fe8027d17b2e8f04055e6c
Zafar Saleem <zafarsaleem3@gmail.com>
2026-06-27 16:40:45 +0100

    fix: box shadow

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import { DevicesAnalytics } from "@/components/2026/6. June/29-30/DevicesAnalytics";
 import styles from "./page.module.css";
-import { ThemeToggle } from "@/components/2026/theme_toggle";
 
 export default function Home() {
   return (
