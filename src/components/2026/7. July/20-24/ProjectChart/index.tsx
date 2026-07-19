a8a7d0a7f088810c570635ea253bc950810a8e0f
Zafar Saleem <zafarsaleem3@gmail.com>
2026-07-19 13:01:23 +0100

    fix: fixed project overview chart

--- src/components/2026/7. July/20-24/ProjectChart/index.tsx
+++ src/components/2026/7. July/20-24/ProjectChart/index.tsx
@@ -1,37 +1,39 @@
 import { CSSProperties } from "react";
 import styles from "./styles.module.css";
 
+const CATREGORIES = [
+  {
+    id: 1,
+    label: "To Do",
+    total: 2,
+    color: "rgba(83, 83, 83, 1)",
+  },
+  {
+    id: 2,
+    label: "In Progress",
+    total: 4,
+    color: "rgba(247, 96, 7, 1)",
+  },
+  {
+    id: 3,
+    label: "Under Review",
+    total: 2,
+    color: "rgba(219, 101, 246, 1)",
+  },
+  {
+    id: 4,
+    label: "Completed",
+    total: 15,
+    color: "rgba(43, 198, 43, 1)",
+  },
+];
+
 const CHART_DATA = {
   title: "Projects Overview",
-  total_projects: 23,
+  total_projects: CATREGORIES.reduce((acc, item) => item.total + acc, 0),
   new_projects: 3,
-  children: [
-    {
-      id: 1,
-      label: "To Do",
-      total: 2,
-      color: "rgba(83, 83, 83, 1)",
-    },
-    {
-      id: 2,
-      label: "In Progress",
-      total: 4,
-      color: "rgba(247, 96, 7, 1)",
-    },
-    {
-      id: 3,
-      label: "Under Review",
-      total: 2,
-      color: "rgba(219, 101, 246, 1)",
-    },
-    {
-      id: 4,
-      label: "Completed",
-      total: 15,
-      color: "rgba(43, 198, 43, 1)",
-    },
-  ]
-}
+  children: CATREGORIES
+};
 
 export const ProjectChart = () => {
   return (
@@ -42,7 +44,7 @@ export const ProjectChart = () => {
         <p className={styles.group}>
           <span className={styles.total_label}>Total Projects</span>
           <span className={styles.total_value}>
-            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-arrow-up-right-icon lucide-arrow-up-right"><path d="M7 7h10v10"/><path d="M7 17 17 7"/></svg>
+            <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-arrow-up-right-icon lucide-arrow-up-right"><path d="M7 7h10v10"/><path d="M7 17 17 7"/></svg>
             +{CHART_DATA.new_projects} new
           </span>
         </p>
@@ -51,10 +53,10 @@ export const ProjectChart = () => {
         className={styles.project_chart}
         style={{
           "--total": CHART_DATA.children.length,
-          "--todo": `${2 / 23 * 100}%`,
-          "--in-progress": `${4 / 23 * 100}%`,
-          "--under-review": `${2 / 23 * 100}%`,
-          "--completed": `${15 / 23 * 100}%`,
+          "--todo": `${2 / CHART_DATA.total_projects * 100}%`,
+          "--in-progress": `${4 / CHART_DATA.total_projects * 100}%`,
+          "--under-review": `${2 / CHART_DATA.total_projects * 100}%`,
+          "--completed": `${15 / CHART_DATA.total_projects * 100}%`,
         } as CSSProperties}
       />
       <ul className={styles.project_categories}>
